10631a97c36a46a0ea52f20646fef35cc610efd1
aujasvi chitkara <aujasvi.chitkara@workday.com>
2015-08-10 22:41:01 -0700

    init

---
+++ front-end-setup.sh
@@ -0,0 +1,11 @@
+#### https://www.youtube.com/watch?v=vkRv0r_tNXY ####
+
+#Download and install node package from https://nodejs.org/
+
+#Install Bower
+# -g flag is for global install
+sudo npm install -g bower
+
+#Install Grunt
+# -g flag is for global install
+sudo npm install -g grunt-cli
